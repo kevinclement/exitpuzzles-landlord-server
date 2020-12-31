478622ffa8ce60803360e0265c15496a9cc50568
Kevin Clement <kevin.clement@gmail.com>
2020-12-31 12:54:48 -0800

    use a state object for tnt

---
+++ TNTState.js
@@ -0,0 +1,39 @@
+module.exports = class TNTState {
+    constructor() {
+        this.version = "unknown"
+        this.gitDate = "unknown"
+        this.buildDate = "unknown"
+
+        this.time = {
+            hours:  0,
+            minutes: 0,
+            seconds: 0
+        }
+
+        this.toggles = {
+            toggle1: false,
+            toggle2: false,
+            toggle3: false,
+            toggle4: false,
+            toggle5: false,
+            failing: false,
+            override: false
+        }
+
+        this.wires = {
+            wire1: false,
+            wire2: false,
+            wire3: false,
+            wire4: false,
+            failing: false,
+            override: false
+        }
+        
+        this.light = false;
+        this.exampleDoor = false;
+        this.key = false;
+        this.password = ''
+        this.overrideWinButton = false;
+        this.finished = false;
+    }
+}
--- manager.tnt-serial.js
+++ manager.tnt-serial.js
@@ -1,4 +1,5 @@
 let Manager = require('./manager.serial')
+let TNTState = require('./TNTState')
 const EVENTS = require('./events');
 
 module.exports = class TntManager extends Manager {
@@ -35,160 +36,142 @@ module.exports = class TntManager extends Manager {
         {
             pattern:/.*status=(.*)/,
             match: (m) => {
+                // variables that track state changes across all options
+                // this is needed so I don't rely on ordering while parsing
+                // I think to do this properly this split foreach would result in a status
+                // object when its done.  then I take that object and update "this" and the database
+                // It would allow me to compare "this" to the parsed object fully and not rely on order
+                
+                // TODO: maybe a TNTState class
+                // TODO: when I switch to this parsedStatus, change hasLight and hasFinished
+                let newState = new TNTState();
+
                 m[1].split(',').forEach((s)=> {
                     let p = s.split(/:(.+)/);
                     switch(p[0]) {
                         case "version": 
-                            this.version = p[1]
+                            newState.version = p[1]
                             break
                         case "gitDate": 
-                            this.gitDate = p[1]
+                            newState.gitDate = p[1]
                             break 
                         case "buildDate": 
-                            this.buildDate = p[1]
+                            newState.buildDate = p[1]
                             break
                             
                         case "time":
                             let tm = /(\d+):(\d+):(\d+)/.exec(p[1]);
                             if (tm) {
-                                this.time.hours = parseInt(tm[1])
-                                this.time.minutes = parseInt(tm[2])
-                                this.time.seconds = parseInt(tm[3])
+                                newState.time.hours = parseInt(tm[1])
+                                newState.time.minutes = parseInt(tm[2])
+                                newState.time.seconds = parseInt(tm[3])
 
                                 // TODO: verify we still need this
-                                //this.timestamp = (new Date()).getTime()
+                                //state.timestamp = (new Date()).getTime()
                             }
                             break
 
                         case "toggle1":
-                            this.toggles.toggle1 = (p[1] === 'true')
+                            newState.toggles.toggle1 = (p[1] === 'true')
                             break
                         case "toggle2":
-                            this.toggles.toggle2 = (p[1] === 'true')
+                            newState.toggles.toggle2 = (p[1] === 'true')
                             break
                         case "toggle3":
-                            this.toggles.toggle3 = (p[1] === 'true')
+                            newState.toggles.toggle3 = (p[1] === 'true')
                             break
                         case "toggle4":
-                            this.toggles.toggle4 = (p[1] === 'true')
+                            newState.toggles.toggle4 = (p[1] === 'true')
                             break
                         case "toggle5":
-                            this.toggles.toggle5 = (p[1] === 'true')
+                            newState.toggles.toggle5 = (p[1] === 'true')
                             break
                         case "togglesFailing":
-                            this.toggles.failing = (p[1] === 'true')
+                            // TODO: AUDIO
+                            // Note: need to take into account override so prob can't just call here
+                            // I could change ordering in the status but that seems fragile
+                            newState.toggles.failing = (p[1] === 'true')
                             break
                         case "overrideToggles":
-                            this.toggles.override = (p[1] === 'true')
+                            newState.toggles.override = (p[1] === 'true')
                             break
                         
                         case "wire1":
-                            this.wires.wire1 = (p[1] === 'true')
+                            newState.wires.wire1 = (p[1] === 'true')
                             break
                         case "wire2":
-                            this.wires.wire2 = (p[1] === 'true')
+                            newState.wires.wire2 = (p[1] === 'true')
                             break
                         case "wire3":
-                            this.wires.wire3 = (p[1] === 'true')
+                            newState.wires.wire3 = (p[1] === 'true')
                             break
                         case "wire4":
-                            this.wires.wire4 = (p[1] === 'true')
+                            newState.wires.wire4 = (p[1] === 'true')
                             break
                         case "wiresFailing":
-                            this.wires.failing = (p[1] === 'true')
+                            // TODO: AUDIO
+                            // Note: need to take into account override so prob can't just call here
+                            newState.wires.failing = (p[1] === 'true')
                             break
                         case "overrideBadWire":
-                            this.wires.override = (p[1] === 'true')
+                            newState.wires.override = (p[1] === 'true')
                             break
                           
                         case "light":
                             let hasLight = (p[1] === 'true')
-                            if (!this.light && hasLight) {
+                            if (!this.state.light && hasLight) {
                                 this.logger.log(this.logPrefix + 'bomb opened.')
                                 this.EE.emit(EVENTS.BOMB_OPENED);
                             }
-                            this.light = hasLight;
+                            newState.light = hasLight;
                             
                             break
                         case "exampleDoor":
-                            this.exampleDoor = (p[1] === 'true')
+                            newState.exampleDoor = (p[1] === 'true')
                             break
                         case "key":
-                            this.key = (p[1] === 'true')
+                            newState.key = (p[1] === 'true')
                             break
                         case "password":
-                            this.password = p[1]
+                            newState.password = p[1]
                             break
                         case "overrideWinButton":
-                            this.overrideWinButton = (p[1] === 'true')
+                            newState.overrideWinButton = (p[1] === 'true')
                             break
 
                         case "finished":
                             let hasFinished = (p[1] === 'true')
-                            if (!this.finished && hasFinished) {
+                            if (!this.state.finished && hasFinished) {
                                 this.logger.log(this.logPrefix + 'bomb finished.')
                                 this.EE.emit(EVENTS.BOMB_FINISHED);
                             }
-                            this.finished = hasFinished
+                            newState.finished = hasFinished
                             break
                     }
                 })
 
                 ref.child('info/build').update({
-                    version: this.version,
-                    date: this.buildDate,
-                    gitDate: this.gitDate
+                    version: newState.version,
+                    date: newState.buildDate,
+                    gitDate: newState.gitDate
                 })
 
                 ref.update({
-                    time: this.time,
-                    toggles: this.toggles,
-                    wires: this.wires,
-                    light: this.light,
-                    exampleDoor: this.exampleDoor,
-                    key: this.key,
-                    password: this.password,
-                    overrideWinButton: this.overrideWinButton,
-                    finished: this.finished,
+                    time: newState.time,
+                    toggles: newState.toggles,
+                    wires: newState.wires,
+                    light: newState.light,
+                    exampleDoor: newState.exampleDoor,
+                    key: newState.key,
+                    password: newState.password,
+                    overrideWinButton: newState.overrideWinButton,
+                    finished: newState.finished,
                 })
             }
         });
 
-        this.version = "unknown"
-        this.gitDate = "unknown"
-        this.buildDate = "unknown"
-
-        this.time = {
-            hours:  0,
-            minutes: 0,
-            seconds: 0
-        }
-
-        this.toggles = {
-            toggle1: false,
-            toggle2: false,
-            toggle3: false,
-            toggle4: false,
-            toggle5: false,
-            failing: false,
-            override: false
-        }
-
-        this.wires = {
-            wire1: false,
-            wire2: false,
-            wire3: false,
-            wire4: false,
-            failing: false,
-            override: false
-        }
-        
-        this.light = false;
-        this.exampleDoor = false;
-        this.key = false;
-        this.password = ''
-        this.overrideWinButton = false;
-        this.finished = false;
+        // start with unknown tnt state
+        this.state = new TNTState();
 
         // now connect to serial
         this.connect()
@@ -212,3 +195,5 @@ module.exports = class TntManager extends Manager {
     //     });
     // }
 }
+
+
