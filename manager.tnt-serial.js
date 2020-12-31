eff63acf5f7cda08067974b58d9feea15441a54d
Kevin Clement <kevin.clement@gmail.com>
2020-12-31 00:40:20 -0800

    use objects in db instead of flat

--- manager.tnt-serial.js
+++ manager.tnt-serial.js
@@ -48,9 +48,9 @@ module.exports = class TntManager extends Manager {
                         case "time":
                             let tm = /(\d+):(\d+):(\d+)/.exec(p[1]);
                             if (tm) {
-                                this.hours   = parseInt(tm[1])
-                                this.minutes = parseInt(tm[2])
-                                this.seconds = parseInt(tm[3])
+                                this.time.hours = parseInt(tm[1])
+                                this.time.minutes = parseInt(tm[2])
+                                this.time.seconds = parseInt(tm[3])
 
                                 // TODO: verify we still need this
                                 //this.timestamp = (new Date()).getTime()
@@ -58,44 +58,44 @@ module.exports = class TntManager extends Manager {
                             break
 
                         case "toggle1":
-                            this.toggle1 = (p[1] === 'true')
+                            this.toggles.toggle1 = (p[1] === 'true')
                             break
                         case "toggle2":
-                            this.toggle2 = (p[1] === 'true')
+                            this.toggles.toggle2 = (p[1] === 'true')
                             break
                         case "toggle3":
-                            this.toggle3 = (p[1] === 'true')
+                            this.toggles.toggle3 = (p[1] === 'true')
                             break
                         case "toggle4":
-                            this.toggle4 = (p[1] === 'true')
+                            this.toggles.toggle4 = (p[1] === 'true')
                             break
                         case "toggle5":
-                            this.toggle5 = (p[1] === 'true')
+                            this.toggles.toggle5 = (p[1] === 'true')
                             break
                         case "togglesFailing":
-                            this.togglesFailing = (p[1] === 'true')
+                            this.toggles.failing = (p[1] === 'true')
                             break
                         case "overrideToggles":
-                            this.overrideToggles = (p[1] === 'true')
+                            this.toggles.override = (p[1] === 'true')
                             break
                         
                         case "wire1":
-                            this.wire1 = (p[1] === 'true')
+                            this.wires.wire1 = (p[1] === 'true')
                             break
                         case "wire2":
-                            this.wire2 = (p[1] === 'true')
+                            this.wires.wire2 = (p[1] === 'true')
                             break
                         case "wire3":
-                            this.wire3 = (p[1] === 'true')
+                            this.wires.wire3 = (p[1] === 'true')
                             break
                         case "wire4":
-                            this.wire4 = (p[1] === 'true')
+                            this.wires.wire4 = (p[1] === 'true')
                             break
                         case "wiresFailing":
-                            this.wiresFailing = (p[1] === 'true')
+                            this.wires.failing = (p[1] === 'true')
                             break
                         case "overrideBadWire":
-                            this.overrideBadWire = (p[1] === 'true')
+                            this.wires.override = (p[1] === 'true')
                             break
                           
                         case "light":
@@ -127,27 +127,9 @@ module.exports = class TntManager extends Manager {
 
                 // TODO: should we split into better subsections?  e.g. toggles\
                 ref.update({
-                    time: 
-                    {
-                        hours: this.hours,
-                        minutes: this.minutes,
-                        seconds: this.seconds
-                    },
-                    toggle1: this.toggle1,
-                    toggle2: this.toggle2,
-                    toggle3: this.toggle3,
-                    toggle4: this.toggle4,
-                    toggle5: this.toggle5,
-                    togglesFailing: this.togglesFailing,
-                    overrideToggles: this.overrideToggles,
-
-                    wire1: this.wire1,
-                    wire2: this.wire2,
-                    wire3: this.wire3,
-                    wire4: this.wire4,
-                    wiresFailing: this.wiresFailing,
-                    overrideBadWire: this.overrideBadWire,
-
+                    time: this.time,
+                    toggles: this.toggles,
+                    wires: this.wires,
                     light: this.light,
                     exampleDoor: this.exampleDoor,
                     key: this.key,
@@ -162,25 +144,31 @@ module.exports = class TntManager extends Manager {
         this.gitDate = "unknown"
         this.buildDate = "unknown"
 
-        this.hours = 0;
-        this.minutes = 0;
-        this.seconds = 0;
+        this.time = {
+            hours:  0,
+            minutes: 0,
+            seconds: 0
+        }
+
+        this.toggles = {
+            toggle1: false,
+            toggle2: false,
+            toggle3: false,
+            toggle4: false,
+            toggle5: false,
+            failing: false,
+            override: false
+        }
+
+        this.wires = {
+            wire1: false,
+            wire2: false,
+            wire3: false,
+            wire4: false,
+            failing: false,
+            override: false
+        }
         
-        this.toggle1 = false;
-        this.toggle2 = false;
-        this.toggle3 = false;
-        this.toggle4 = false;
-        this.toggle5 = false;
-        this.togglesFailing = false;
-        this.overrideToggles = false;
-
-        this.wire1 = false;
-        this.wire2 = false;
-        this.wire3 = false;
-        this.wire4 = false;
-        this.wiresFailing = false;
-        this.overrideBadWire = false;
-
         this.light = false;
         this.exampleDoor = false;
         this.key = false;
