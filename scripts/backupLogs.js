0513167297d36d6490759d872e40e70ecc766930
Kevin Clement <kevin.clement@gmail.com>
2021-01-07 01:15:47 -0800

    move to function

--- scripts/backupLogs.js
+++ scripts/backupLogs.js
@@ -11,67 +11,64 @@ if (fs.existsSync(CONFIG_FNAME)) {
 }
 CONFIG.runs++;
 
-let OUT_FNAME = `${ROOT}/logs.${pad(CONFIG.runs, 4)}.json`;
-
-// should never happen, but lets protect
-if (fs.existsSync(OUT_FNAME)) {
-    console.log(`ERROR: file ${OUT_FNAME} already exists, I dont want to blow it away, Exiting.`);
-    process.exit();
-}
-
-console.log(`Backup Logs to ${OUT_FNAME}...`)
-
 let LIMIT = 3;
 let TIMEOUT_MS = 3200;
-
-let dbs = ['landlord'];
-let tables = ['tmpLog'];
-
+let DB_ROOT_PATH = 'landlord/tmpLog'
 let data = {}
 let timeout = null
-dbs.forEach(db => {
-    tables.forEach(table => {
-        console.log(`  dumping ${db}/${table}...`)
 
-        let logsRef = fb.db.ref(`/${db}/${table}`).orderByKey().limitToFirst(LIMIT);
+dumpLogs(DB_ROOT_PATH, CONFIG.lastKey, CONFIG.runs)
 
-        if (CONFIG.lastKey && CONFIG.lastKey != '') {
-            logsRef = logsRef.startAt(CONFIG.lastKey)
+function dumpLogs(logsPath, lastKey, runNumber) {
+    let fName = `${ROOT}/logs.${pad(runNumber, 4)}.json`;
+    
+    // should never happen, but lets protect
+    if (fs.existsSync(fName)) {
+        console.log(`ERROR: file ${fName} already exists, I dont want to blow it away, Exiting.`);
+        process.exit();
+    }
+
+    console.log(`Backup Logs to ${fName}...`)
+    console.log(`  dumping ${logsPath}...`)
+
+    let logsRef = fb.db.ref(`/${logsPath}`).orderByKey().limitToFirst(LIMIT);
+
+    if (CONFIG.lastKey && CONFIG.lastKey != '') {
+        logsRef = logsRef.startAt(CONFIG.lastKey)
+    }
+
+    logsRef.on('child_added', (s) => {
+        let log = s.val()
+
+        // if lastKey in config, skip first element since we'll already have it in last backup
+        if (s.key == CONFIG.lastKey) {
+            console.log(`skipping already stored ${s.key}...`)
+            return;    
         }
 
-        logsRef.on('child_added', (s) => {
-            let log = s.val()
+        console.log(`${s.key}  : ${log.timestamp}  : ${log.data}`)
+        CONFIG.lastKey = s.key
 
-            // if lastKey in config, skip first element since we'll already have it in last backup
-            if (s.key == CONFIG.lastKey) {
-                console.log(`skipping already stored ${s.key}...`)
-                return;    
-            }
+        // add to our json object
+        data[s.key] = s.toJSON();
 
-            console.log(`${s.key}  : ${log.timestamp}  : ${log.data}`)
-            CONFIG.lastKey = s.key
+        if (timeout) {
+            clearTimeout(timeout)
+        }
 
-            // add to our json object
-            data[s.key] = s.toJSON();
-
-            if (timeout) {
-                clearTimeout(timeout)
-            }
-
-            // Nerd.  I don't know when to tell we've hit the end other then just setting
-            // a timer to timeout.  I cant just count limit because we might not reach that many
-            // don't know of a better way right now
-            timeout = setTimeout(() =>
-            {
-                fs.mkdirSync(ROOT, { recursive: true })
-                fs.writeFileSync(CONFIG_FNAME, JSON.stringify(CONFIG, null, 2))
-                fs.writeFileSync(OUT_FNAME, JSON.stringify(data, null, 2))
-                console.log("DONE.")
-                process.exit();
-            }, TIMEOUT_MS);
-        })
-    });
-});
+        // Nerd.  I don't know when to tell we've hit the end other then just setting
+        // a timer to timeout.  I cant just count limit because we might not reach that many
+        // don't know of a better way right now
+        timeout = setTimeout(() =>
+        {
+            fs.mkdirSync(ROOT, { recursive: true })
+            fs.writeFileSync(CONFIG_FNAME, JSON.stringify(CONFIG, null, 2))
+            fs.writeFileSync(fName, JSON.stringify(data, null, 2))
+            console.log("DONE.")
+            process.exit();
+        }, TIMEOUT_MS);
+    })
+}
 
 function getRootFolder(rootDir) {
     let today = new Date
