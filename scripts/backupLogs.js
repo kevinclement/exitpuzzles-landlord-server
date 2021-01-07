bff3438679e7c246fe14710ae8044363d733f64e
Kevin Clement <kevin.clement@gmail.com>
2021-01-07 01:08:00 -0800

    filename based on runs

--- scripts/backupLogs.js
+++ scripts/backupLogs.js
@@ -1,40 +1,55 @@
 let fb = new (require('../firebase'))
 let fs = require('fs')
+var crypto = require('crypto');
 
 let ROOT = getRootFolder('logs')
-let cfgName = `${ROOT}/config.json`
-let CONFIG = {}
-if (fs.existsSync(cfgName)) {
-    CONFIG = require(`${ROOT}/config.json`)
+let CONFIG_FNAME = `${ROOT}/config.json`
+
+let CONFIG = { runs: 0 }
+if (fs.existsSync(CONFIG_FNAME)) {
+    CONFIG = require(`./${CONFIG_FNAME}`)
+}
+CONFIG.runs++;
+
+let OUT_FNAME = `${ROOT}/logs.${pad(CONFIG.runs, 4)}.json`;
+
+// should never happen, but lets protect
+if (fs.existsSync(OUT_FNAME)) {
+    console.log(`ERROR: file ${OUT_FNAME} already exists, I dont want to blow it away, Exiting.`);
+    process.exit();
 }
 
-console.log(`Backup Logs ...`)
+console.log(`Backup Logs to ${OUT_FNAME}...`)
 
 let LIMIT = 3;
 let TIMEOUT_MS = 3200;
 
 let dbs = ['landlord'];
 let tables = ['tmpLog'];
-let startKey = '-Lnj1MkZHOWMg_PYqVuD';
 
-let refCount = 0
-let dumpCount = 0
 let data = {}
-let lastKey = ''
 let timeout = null
 dbs.forEach(db => {
     tables.forEach(table => {
-        refCount++;
-        let logsRef = fb.db.ref(`/${db}/${table}`).orderByKey().startAt(startKey).limitToFirst(LIMIT);
         console.log(`  dumping ${db}/${table}...`)
+
+        let logsRef = fb.db.ref(`/${db}/${table}`).orderByKey().limitToFirst(LIMIT);
+
+        if (CONFIG.lastKey && CONFIG.lastKey != '') {
+            logsRef = logsRef.startAt(CONFIG.lastKey)
+        }
+
         logsRef.on('child_added', (s) => {
             let log = s.val()
-            console.log(`${s.key}  : ${log.timestamp}  : ${log.data}`)
-            lastKey = s.key
 
-            let tableFolder = `${ROOT}/${db}`
-            let fName = `${tableFolder}/${table}.json`
-            fs.mkdirSync(tableFolder, { recursive: true })
+            // if lastKey in config, skip first element since we'll already have it in last backup
+            if (s.key == CONFIG.lastKey) {
+                console.log(`skipping already stored ${s.key}...`)
+                return;    
+            }
+
+            console.log(`${s.key}  : ${log.timestamp}  : ${log.data}`)
+            CONFIG.lastKey = s.key
 
             // add to our json object
             data[s.key] = s.toJSON();
@@ -48,7 +63,9 @@ dbs.forEach(db => {
             // don't know of a better way right now
             timeout = setTimeout(() =>
             {
-                fs.writeFileSync(fName, JSON.stringify(data, null, 2))
+                fs.mkdirSync(ROOT, { recursive: true })
+                fs.writeFileSync(CONFIG_FNAME, JSON.stringify(CONFIG, null, 2))
+                fs.writeFileSync(OUT_FNAME, JSON.stringify(data, null, 2))
                 console.log("DONE.")
                 process.exit();
             }, TIMEOUT_MS);
@@ -62,8 +79,14 @@ function getRootFolder(rootDir) {
     let day = today.getUTCDate()
 
     // zero pad folders
-    month = month < 10 ? "0" + month : month;
-    day = day < 10 ? "0" + day : day;
+    month = pad(month, 2)
+    day = pad(day, 2)
 
     return `${rootDir}/${today.getUTCFullYear()}/${month}-${day}`
+}
+
+function pad(n, width, z) {
+    z = z || '0';
+    n = n + '';
+    return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
 }
